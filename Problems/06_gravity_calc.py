d6e54026f6bab148f1bb941d0b6d71ee40c0bdbf
1ssorenn <rsoren@fwparker.org>
2020-03-05 14:08:26 -0600

    Added imports notes

--- Problems/06_gravity_calc.py
+++ Problems/06_gravity_calc.py
@@ -13,6 +13,46 @@
 # (4pts) contains exceptions for any potential errors (value and dividebyzero).
 # (2pts) keeps asking for inputs until they are valid (see while loop from notes)
 # (3pts) calculates the force of gravity in Newtons and print the result to the user in scientific notation to two decimals.
+done = False
+done2 = False
+done3 = False
+
+G = 6.67e-11
+
+
+while not done:
+    try:
+        m1 = float(input("\nEnter mass 1(kg): "))
+        done = True
+    except ValueError as e:
+        print("Invalid,", e)
+        print("Enter a new value.")
+
+    while not done2:
+        try:
+            m2 = float(input("\nEnter mass 2(kg): "))
+            done2 = True
+        except ValueError as e:
+            print("Invalid,", e)
+            print("Enter a new value.")
+
+
+        while not done3:
+            try:
+                r = float(input("\nEnter distance(meters) between objects: "))
+                F = G * (m1 * m2) / r ** 2
+                done3 = True
+            except ValueError as e:
+                print("Invalid,", e)
+                print("Enter a new value.")
+            except ZeroDivisionError as y:
+                print("Error:", y)
+                print("Enter a new value")
+
+
+print("{:.3}".format(F))
+
+
 
 
 
