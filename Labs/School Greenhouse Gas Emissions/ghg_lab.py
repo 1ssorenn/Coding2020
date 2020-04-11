de0ad85f174b04f292d4dc4e459a59298c7b1aaa
1ssorenn <rsoren@fwparker.org>
2020-04-10 23:48:09 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Problems/Sorting/05_sorting_problems.py

--- Labs/School Greenhouse Gas Emissions/ghg_lab.py
+++ Labs/School Greenhouse Gas Emissions/ghg_lab.py
@@ -38,4 +38,90 @@ Maybe you can try one of the following or think up your own:
 Note 2:  This is a tough assignment to do on your own.  Do your best with what you have.
 '''
 
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+color = []
+valid_data = []
+names = []
+url = "https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD"
 
+
+
+
+# Def get info
+def rec_info(url):
+    with requests.Session() as s:
+        d = s.get(url)
+        c = d.content.decode('utf-8')
+        r = csv.reader(c.splitlines(), delimiter=',')
+        my_list = list(r)
+    return my_list
+info = rec_info("https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD")
+header = info.pop(0)
+print(header)
+
+# Index
+index1 = header.index("Total GHG Emissions (Metric Tons CO2e)")
+index2 = header.index("Primary Property Type")
+index3 = header.index("Gross Floor Area - Buildings (sq ft)")
+
+# find valid data
+for building in info:
+    try:
+        int(building[index1])
+        int(building[index3])
+        name = building[2]
+        if building[0] == "2018" and building[index2] == "K-12 School":
+            valid_data.append(building)
+            names.append(name)
+    except:
+        pass
+
+plt.figure(1, tight_layout=True)
+
+
+ghg = [int(x[index1]) for x in valid_data]
+ghg.sort()
+
+space = [int(x[index3]) for x in valid_data]
+space.sort()
+
+
+
+
+p = np.polyfit(space, ghg, 1)
+print(p)
+
+x = [x for x in range(450000)]
+y = [p[0] * y + p[1] for y in x]
+plt.plot(x, y, c="black")
+plt.xlabel("Gross Floor Area (sq ft)")
+plt.ylabel("Total GHG Emissions (Metric Tons CO2e)")
+plt.title("Emissions vs. Square Footage in Chicago Schools (2018)")
+for name in names:
+    if name == "Latin School of Chicago Upper School":
+        name_index = names.index(name)
+        plt.annotate("Latin", xy=(space[name_index], ghg[name_index]))
+
+
+
+ten_percent = int(len(ghg) * 0.1)
+
+ghg_bottom10 = [x for x in ghg[:ten_percent]]
+ghg_top10 = [x for x in ghg[-ten_percent:]]
+print(ghg_top10)
+print(ghg_bottom10)
+
+
+# Set for Color of points
+for building in ghg:
+    if building in ghg_top10:
+        color.append("red")
+    elif building in ghg_bottom10:
+        color.append("green")
+    else:
+        color.append("blue")
+plt.scatter(space, ghg, alpha=0.5, c=color)
+plt.show()
